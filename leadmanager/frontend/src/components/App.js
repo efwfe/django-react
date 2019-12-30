2724d302ae6319005885a04bbe10e59116f7ad34
zhangdaihui <spartazhangzhan@gmail.com>
2019-12-30 11:00:11 +0800

    simple react-redux

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import ReactDom from "react-dom"
 import Header from "./layout/Header"
 
+import {Provider} from 'react-redux';
+import store from "../store";
+import Dashboard from './leads/Dashboard';
+
+
 class App extends Component {
     render() { 
+        
         return (
+            <Provider store={store}>
             <Header />
+            <div className="container">
+                <Dashboard />
+            </div>
+            </Provider>
           );
     }
 }
